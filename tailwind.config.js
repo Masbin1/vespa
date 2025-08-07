15084a43c114d48730b68dbd47897a2fb3c1c0a1
masbin <m.bintangkhatulistiwa@gmail.com>
2025-08-07 20:54:23 +0700

    asdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        archivoBlack: ['"Archivo Black"', 'sans-serif'],
+        barlow: ['"Barlow"', 'sans-serif'],
+      },
     },
   },
   plugins: [],
